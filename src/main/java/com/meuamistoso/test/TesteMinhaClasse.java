dbda676bdea1081c5bec5f0767ae4db122a603a4
Napoleon Michel Degreas <degreasnapoleon@gmail.com>
2023-11-23 22:43:29 -0300

    tamo fudido

---
+++ src/main/java/com/meuamistoso/test/TesteMinhaClasse.java
@@ -0,0 +1,21 @@
+package com.meuamistoso.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TesteMinhaClasse {
+
+    @Test
+    public void testeMetodo1() {
+        // Coloque seu código de teste aqui
+        assertTrue(true); // Exemplo de assertiva
+    }
+
+    @Test
+    public void testeMetodo2() {
+        // Coloque outro código de teste aqui
+        assertEquals(2, 1 + 1); // Exemplo de assertiva
+    }
+}
